4738ccd0d754cf74f4e2d2a280aff1fa06dd7d8e
lazejess <90428277+lazejess@users.noreply.github.com>
2021-11-18 10:20:33 -0500

    updates

--- soundboard.js
+++ soundboard.js
@@ -1,18 +1,8 @@
-// Make sure all whole page has loaded before executing the script
-window.onload = function() {
-    const hamburger = document.querySelector(".mobile-trigger"); // Set a constant variable for the hamburger link
-    const navigation = document.querySelector(".desktop-links"); // Set a constant variable for the unordered nav list
-    
-    // Toggle function checks the state of the navigation constant and displays or hides the links
-    function toggle() {
-      if (window.getComputedStyle(navigation).display === "none") {
-        navigation.style.display = "block";
-      } else {
-        navigation.style.display = "none";
-      }
-    }
-    
-    // Create an "even listener" to watch for clicks on the hamburger link
-    // When it detects a click it executes the toggle() function above
-    hamburger.addEventListener("click", toggle);
-  };
+const sounds= ["Applause", "Cheering", "Booing"];
+
+sounds.forEach((sound) => {
+    const btn = document.createElement("button");
+    btn.classList.add('btn');
+    btn.innerText = sound;
+    document.getElementByID("buttons").appendChuld(btn);
+});
