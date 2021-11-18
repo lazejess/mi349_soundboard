63f6d24447d868a0855689d6fd3d84f46cddd958
lazejess <90428277+lazejess@users.noreply.github.com>
2021-11-18 10:30:58 -0500

    updates

--- soundboard.js
+++ soundboard.js
@@ -4,5 +4,16 @@ sounds.forEach((sound) => {
     const btn = document.createElement("button");
     btn.classList.add('btn');
     btn.innerText = sound;
+    btn.addEventListener("click", ()=>{
+        stopsound();
+        document.getElementById(sound).onplay();
+    })
     document.getElementByID("buttons").appendChuld(btn);
 });
+function stopsound(){
+    sounds.forEach(sound => {
+        const audio = document.getElementById(sound);
+        audio.pause();
+        audio.currentTime(0);
+    });
+}
