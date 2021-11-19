6118e66c19332f19ace2183fca990f8b7f7ab3a0
lazejess <90428277+lazejess@users.noreply.github.com>
2021-11-18 22:25:44 -0500

    updates

--- soundboard.js
+++ soundboard.js
@@ -4,7 +4,24 @@ const sounds= [
     "Booing",
 ];
 
-function play() {
-    var audio = document.getElementById("audio");
-    audio.play();
-  }
+sounds.forEach((sound) => {
+    const btn = document.createElement("button");
+    btn.classList.add("btn");
+    
+    btn.innerText = sound;
+
+    btn.addEventListener("click", () => {
+        stopSongs();
+        document.getElementById(sound).play();
+    });
+
+    document.getElementById("buttons").appendChild(btn);
+});
+
+function stopSongs() {
+    sounds.forEach((sound) => {
+        const song = document.getElementById(sound);
+        song.pause();
+        song.currentTime = 0;
+    })
+}
