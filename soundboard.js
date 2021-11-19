15bdfeafd96f01150e8a15c609dfdd696099c45c
lazejess <90428277+lazejess@users.noreply.github.com>
2021-11-18 20:50:21 -0500

    updates

--- soundboard.js
+++ soundboard.js
@@ -18,10 +18,3 @@ sounds.forEach((sound) => {
     document.getElementById('buttons').appendChild(btn);
 })
 
-function stopSongs() {
-    sounds.forEach((sound) => {
-        const song = document.getElementById(sound);
-        song.pause();
-        song.currentTime = 0;
-    })
-}
